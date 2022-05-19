0724725aed081961e710e7a1f65ce75c33892fbf
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-05-19 02:32:44 -0700

    🏆 add flask empty check and keypress

--- poe-hp.py
+++ poe-hp.py
@@ -1,6 +1,10 @@
 import numpy as np
+from random import uniform
+from time import sleep
 import pyautogui as pag
 import cv2 as opencv
+import pydirectinput as pdi
+import pynput
 from win32gui import GetWindowText, GetForegroundWindow
 '''
 Health globe of PoE at around 30% life is y coordinate ~1000
@@ -13,18 +17,28 @@ full_life_mean = np.array([118.164, 13.866, 21.914])
 delta = 48.1
 is_low_life = False
 active_window_preferred = 'Path of Exile'
+# 328 1058, 16, 16, 17 (flask empty)
+flask_empty_pos = (328, 1058)
+flask_empty_color = [16,16,17]
 
 def checkCurrentWindow() -> bool:
     active = GetForegroundWindow()
     title = GetWindowText(active)
     return title in active_window_preferred
 
+def checkFlaskEmpty() -> bool:
+    grab = pag.screenshot(region=(*flask_empty_pos,1,1))
+    img = np.array(grab)[0][0]
+    return all([img[i] == flask_empty_color[i] for i in range(3)])
+
 def checkIfLowLife():
     global is_low_life
     while 1:
+        # pag.displayMousePosition();
         if not checkCurrentWindow():
             print('\rPoE not active window...', end = '                     ', flush=True); continue
-        print('\rIs Low Life? : ', 'Yes' if is_low_life else 'No ', flush=True, end = '                     ')
+        print('\rIs Low Life? : ', 'Yes' if is_low_life else 'No ', flush=True, end = '')
+        print(f'   | Flask Empty : ', 'Yes' if checkFlaskEmpty() else 'No', flush=True, end='   ')
         grab = pag.screenshot(region=
             (
                 X_RANGE[0], 
@@ -38,6 +52,7 @@ def checkIfLowLife():
         if any(res :=[abs(mean[i]-full_life_mean[i]) > delta for i in range(3)]):
             is_low_life = True
             pag.press('1')
+            sleep(.06 + uniform(-.02123, .011738)) # dont spam flask
             # [print(abs(mean[i]-full_life_mean[i])) for (i, x) in enumerate(res) if x]
             # print('Delta hit!~', flush=True)
         else: is_low_life = False
---
+++ test_press.py
@@ -0,0 +1,5 @@
+import pyautogui as pag
+from time import sleep
+
+sleep(1.5)
+pag.press('1')
